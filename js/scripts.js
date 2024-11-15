6aaa976422f7e2f38f5b86b88eb63e68ca0b90d1
Eika-Koop <jzuazo@eika.es>
2024-11-15 11:32:11 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,17 @@ document.addEventListener('DOMContentLoaded', function () {
     const loginForm = document.getElementById('loginForm');
 
     loginForm.addEventListener('submit', function (event) {
-        event.preventDefault();
+        event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada.
 
         const username = document.getElementById('username').value.trim();
         const password = document.getElementById('password').value;
 
+        // Validación de credenciales
         if (username === 'EikaRRHH' && password === 'Pepinillo20*') {
+            // Redirigir a la página de éxito
             window.location.href = 'success.html';
         } else {
+            // Mostrar un mensaje de error si las credenciales son incorrectas
             alert('Erabiltzailea edo pasahitza ez da zuzena!');
         }
     });
